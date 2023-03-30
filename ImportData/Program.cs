f75c7e9490eb9262ddf75c6c3e28202060d20422
Paulo Costa Carvalho <paulo@pcarvalho.com>
2023-03-30 14:26:15 -0300

    jhk

--- ImportData/Program.cs
+++ ImportData/Program.cs
@@ -14,15 +14,7 @@ namespace SequenceAssemblerLogic
             // P A U L O C O S T A C A R V A L H O
             // 1 2 3 4 5 6 7 8 9 1 2 3 4 5 7 8 7 6
 
-            Stopwatch sw = Stopwatch.StartNew();
-
-            sw.Start();
-            for (int i = 0; i < 1000000; i++)
-            {
-                var results = NovorParser.GetSubSequences2("PAULOCOSTACARVALHO", new List<int>() { 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 7, 8, 7, 6 }, 5, 3);
-            }
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed.ToString());
+            var results = NovorParser.GetSubSequences2("PAULOCOSTACARVALHO", new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 7, 8, 7, 6 }, 5, 3);
 
 
             Console.WriteLine("Done");
