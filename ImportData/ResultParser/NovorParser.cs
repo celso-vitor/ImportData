ca8894278850fe7756b7e5be63ddc9ded4ca9311
Celso <Celso@CELSO-NOTEBOOK>
2023-05-14 19:26:35 -0300

    Atualizações

--- ImportData/ResultParser/NovorParser.cs
+++ ImportData/ResultParser/NovorParser.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace SequenceAssemblerLogic.ResultParser
 {
--- SequenceAssemblerGUI/MainWindow.xaml.cs
+++ SequenceAssemblerGUI/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using SequenceAssemblerLogic.ResultParser;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Data;
 using System.Formats.Asn1;
 using System.IO;
@@ -13,6 +14,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
@@ -41,37 +43,59 @@ namespace SequenceAssemblerGUI
 
         private void MenuItemImportResults_Click(object sender, RoutedEventArgs e)
         {
+
             VistaFolderBrowserDialog folderBrowserDialog = new VistaFolderBrowserDialog();
             folderBrowserDialog.Multiselect = true;
 
             if ((bool)folderBrowserDialog.ShowDialog())
             {
                 DataTable dtDenovo = new DataTable();
+                dtDenovo.Columns.Add("Folder", typeof(string)); // Adicione uma nova coluna para o nome da pasta
                 dtDenovo.Columns.Add("Sequences Peptides DeNovo", typeof(string));
+                dtDenovo.Columns.Add("Score Peptides DeNovo", typeof(double));
+                dtDenovo.Columns.Add("ScanNumber Peptides DeNovo", typeof(int));
 
                 DataTable dtPsm = new DataTable();
-                dtPsm.Columns.Add("Sequences Peptides PSM", typeof(string)); // Adicione as colunas necessárias para os registros de Psm
+                dtPsm.Columns.Add("Folder", typeof(string)); // Adicione uma nova coluna para o nome da pasta
+                dtPsm.Columns.Add("Sequences Peptides PSM", typeof(string));
+                dtPsm.Columns.Add("Score Peptides PSM", typeof(double));
+                dtPsm.Columns.Add("ScanNumber Peptides PSM", typeof(int));
 
-                foreach (string folderPath in folderBrowserDialog.SelectedPaths)
+                foreach(string folderPath in folderBrowserDialog.SelectedPaths)
                 {
+                    DirectoryInfo mainDir = new DirectoryInfo(folderPath);
+                    string folderName = mainDir.Name; // Obter o nome da pasta selecionada
+
+                    foreach (DirectoryInfo subDir in mainDir.GetDirectories())
+                    {
+                        folderName += $",{subDir.Name}"; // Adicione o nome da subpasta separado por vírgula
+                    }
+
                     novorParser = new();
-                    novorParser.LoadNovorUniversal(new DirectoryInfo(folderPath));
+                    novorParser.LoadNovorUniversal(mainDir);
 
                     foreach (var denovo in novorParser.DictDenovo.Values.SelectMany(x => x))
                     {
                         DataRow row = dtDenovo.NewRow();
+                        row["Folder"] = folderName; // Defina o valor da nova coluna para o nome da pasta
                         row["Sequences Peptides DeNovo"] = denovo.Peptide;
+                        row["Score Peptides DeNovo"] = denovo.Score;
+                        row["ScanNumber Peptides DeNovo"] = denovo.ScanNumber;
                         dtDenovo.Rows.Add(row);
                     }
 
                     foreach (var psm in novorParser.DictPsm.Values.SelectMany(x => x))
                     {
                         DataRow row = dtPsm.NewRow();
-                        row["Sequences Peptides PSM"] = psm.Peptide; // Preencha as colunas necessárias para os registros de Psm
+                        row["Folder"] = folderName; // Defina o valor da nova coluna para o nome da pasta
+                        row["Sequences Peptides PSM"] = psm.Peptide;
+                        row["Score Peptides PSM"] = psm.Score;
+                        row["ScanNumber Peptides PSM"] = psm.ScanNumber;
                         dtPsm.Rows.Add(row);
                     }
                 }
 
+         
                 DataView dvDenovo = new DataView(dtDenovo);
                 DataGridDeNovo.ItemsSource = dvDenovo;
 
@@ -129,8 +153,50 @@ namespace SequenceAssemblerGUI
             PlotViewEnzymeEfficiency.Model = plotModel1;
 
         }
+        private void UpdateDataView(DataView dataView, Dictionary<string, List<DeNovoRegistry>> deNovoDict, Dictionary<string, List<PsmRegistry>> psmDict)
+        {
+            if (dataView == null) return;
 
-  
+            DataTable dataTable = dataView.Table;
+            dataTable.Rows.Clear();
+
+            if (deNovoDict != null)
+            {
+                foreach (var kvp in deNovoDict)
+                {
+                    string folderName = kvp.Key;
+
+                    foreach (var denovo in kvp.Value)
+                    {
+                        DataRow row = dataTable.NewRow();
+                        row["Folder"] = folderName;
+                        row["Sequences Peptides DeNovo"] = denovo.Peptide;
+                        row["Score Peptides DeNovo"] = denovo.Score;
+                        row["ScanNumber Peptides DeNovo"] = denovo.ScanNumber;
+                        dataTable.Rows.Add(row);
+                    }
+                }
+            }
+
+            if (psmDict != null)
+            {
+                foreach (var kvp in psmDict)
+                {
+                    string folderName = kvp.Key;
+
+                    foreach (var psm in kvp.Value)
+                    {
+                        DataRow row = dataTable.NewRow();
+                        row["Folder"] = folderName;
+                        row["Sequences Peptides PSM"] = psm.Peptide;
+                        row["Score Peptides PSM"] = psm.Score;
+                        row["ScanNumber Peptides PSM"] = psm.ScanNumber;
+                        dataTable.Rows.Add(row);
+                    }
+                }
+            }
+        }
+
 
         private void UpdateGeneral()
         {
@@ -153,11 +219,11 @@ namespace SequenceAssemblerGUI
 
             // Apply filters to the filtered dictionaries
 
-           // int denovoMinSequeceLength = (int)IntegerUpDownDeNovoMinLength.Value;
-           // NovorParser.FilterDictMinLengthDeNovo(denovoMinSequeceLength, deNovoDictTemp);
+            int denovoMinSequeceLength = (int)IntegerUpDownDeNovoMinLength.Value;
+            NovorParser.FilterDictMinLengthDeNovo(denovoMinSequeceLength, deNovoDictTemp);
 
-            //int denovoMaxSequeceLength = (int)IntegerUpDownDeNovoMaxLength.Value;
-           // NovorParser.FilterDictMaxLengthDeNovo(denovoMaxSequeceLength, deNovoDictTemp);
+            int denovoMaxSequeceLength = (int)IntegerUpDownDeNovoMaxLength.Value;
+            NovorParser.FilterDictMaxLengthDeNovo(denovoMaxSequeceLength, deNovoDictTemp);
 
             int psmMinSequenceLength = (int)IntegerUpDownPSMMinLength.Value;
             NovorParser.FilterDictMinLengthPSM(psmMinSequenceLength, psmDictTemp);
@@ -167,6 +233,9 @@ namespace SequenceAssemblerGUI
 
             int filterPsmSocore = (int)IntegerUpDownPSMScore.Value;
             NovorParser.FilterSequencesByScorePSM(filterPsmSocore, psmDictTemp);
+
+            UpdateDataView(DataGridDeNovo.ItemsSource as DataView, deNovoDictTemp, null);
+            UpdateDataView(DataGridPSM.ItemsSource as DataView, null, psmDictTemp);
 
             // Update the plot with the filtered data
             UpdatePlot();
@@ -184,9 +253,10 @@ namespace SequenceAssemblerGUI
         private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
         {
             UpdateGeneral();
+
         }
 
-        
+       
     }
        
 }
