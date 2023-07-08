35ec5edcebf79e419e94b7108d147d95ab54ebc7
Celso <Celso@CELSO-NOTEBOOK>
2023-07-08 20:27:58 -0300

    Atualizações

--- ImportData/Program.cs
+++ ImportData/Program.cs
@@ -5,8 +5,6 @@ using System.Linq;
 
 public class Program
 {
-
-
     public static void Main()
     {
         // Inicializa uma lista de sequências de strings
@@ -18,50 +16,20 @@ public class Program
 
         // Coloca todas as listas em uma lista de listas
         Dictionary<string, List<string>> listOfLists = new Dictionary<string, List<string>>
+    {
+        {"trypsina", trypsina},
+        {"pepsina", pepsina},
+        {"lysc", lysc},
+        {"v3", v3},
+        {"micro", micro}
+    };
+
+        List<(string ID, int Gain)> sequenceGains = Useful.GenerateOrderedGains(listOfLists);
+
+        // Imprimir os ganhos ordenados
+        foreach (var entry in sequenceGains)
         {
-            {"trypsina", trypsina},
-            {"pepsina", pepsina},
-            {"lysc", lysc},
-            {"v3", v3},
-            {"micro", micro}
-        };
-
-        // Encontrar a lista com o maior número de sequências
-        string nameOfLargestList = "";
-        int maxCount = 0;
-
-        foreach (var entry in listOfLists)
-        {
-            if (entry.Value.Count > maxCount)
-            {
-                maxCount = entry.Value.Count;
-                nameOfLargestList = entry.Key;
-            }
-        }
-
-        List<string> largestList = listOfLists[nameOfLargestList];
-
-        Console.WriteLine($"{nameOfLargestList} = {string.Join(", ", largestList)}");
-
-        // Encontrar as sequências que não estão presentes na maior lista e guardar em uma lista auxiliar
-        List<Tuple<string, List<string>>> complementarySequences = new List<Tuple<string, List<string>>>();
-
-        foreach (var entry in listOfLists)
-        {
-            if (entry.Key != nameOfLargestList)
-            {
-                var complementary = entry.Value.Except(largestList).ToList();
-                complementarySequences.Add(Tuple.Create(entry.Key, complementary));
-            }
-        }
-
-        // Ordenar as sequências complementares por quantidade de elementos
-        complementarySequences.Sort((x, y) => y.Item2.Count.CompareTo(x.Item2.Count));
-
-        // Exibir as sequências complementares ordenadas
-        foreach (var entry in complementarySequences)
-        {
-            Console.WriteLine($"{entry.Item1} = {string.Join(", ", entry.Item2)}");
+            Console.WriteLine($"ID: {entry.ID}, Gain: {entry.Gain}");
         }
     }
 }
---
+++ ImportData/Useful.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceAssemblerLogic
+{
+    public static class Useful
+    {
+        public static List<(string ID, int Gain)> GenerateOrderedGains(Dictionary<string, List<string>> listOfLists)
+        {
+            // Identifica a lista com o maior número de sequências
+            string nameOfLargestList = "";
+            int maxCount = 0;
+
+            foreach (var entry in listOfLists)
+            {
+                if (entry.Value.Count > maxCount)
+                {
+                    maxCount = entry.Value.Count;
+                    nameOfLargestList = entry.Key;
+                }
+            }
+
+            List<string> largestList = listOfLists[nameOfLargestList];
+
+            // Encontra as sequências que não estão presentes na maior lista
+            List<(string ID, int Gain)> sequenceGains = new List<(string ID, int Gain)>();
+
+            foreach (var entry in listOfLists)
+            {
+                if (entry.Key != nameOfLargestList)
+                {
+                    var complementary = entry.Value.Except(largestList).ToList();
+                    sequenceGains.Add((entry.Key, complementary.Count));
+                }
+            }
+
+            // Ordena a lista de ganhos
+            sequenceGains.Sort((x, y) => y.Gain.CompareTo(x.Gain));
+
+            return sequenceGains;
+        }
+    }
+}
