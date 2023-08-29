ba6d42bd388d1b43d23013a13e12ffa265d865ca
Celso <Celso@CELSO-NOTEBOOK>
2023-08-29 14:02:22 -0300

    atualizações

--- ImportData/Program.cs
+++ ImportData/Program.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.IO;
-using static System.Net.Mime.MediaTypeNames;
 using System.Xml;
 
 public class Program
@@ -14,29 +13,63 @@ public class Program
         List<FASTA> alignments = FastaParser.ParseFastaFile("output.txt");
         Console.WriteLine(alignments.Count + " alignments read");
 
-        List<(string word, int startPos)> contigPos = new();
-        
         if (alignments.Count > 1)
         {
+            // Reference String
+            string reference = alignments[0].Sequence;
+
             for (int i = 1; i < alignments.Count; i++)
             {
+                // The current alignment and its name (Header)
+                string contig = alignments[i].Sequence;
+                string contigName = alignments[i].ID;
 
+                // Initialize positions
+                int contigPos = 0;
+                int refPos = 0;
+
+                // To store the positions of the contigs
+                List<(string word, int startPos)> contigPosList = new();
+
+                string word = "";
+                for (int j = 0; j < contig.Length; j++)
+                {
+                    if (contig[j] != '-')
+                    {
+                        word += contig[j];
+                        contigPos++;
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrEmpty(word))
+                        {
+                            contigPosList.Add((word, refPos + 1 - word.Length));
+                            word = "";
+                        }
+                    }
+
+                    if (j < reference.Length && reference[j] != '-')
+                    {
+                        refPos++;
+                    }
+                }
+
+                // Print the positions found
+                Console.WriteLine($"{contigName}:");
+                foreach (var (wordFound, startPosition) in contigPosList)
+                {
+                    Console.WriteLine($"{wordFound}, Position: {startPosition}");
+                }
             }
         }
-        
 
         Console.WriteLine("Done");
-
-
-
     }
 }
-    
 
 
 
 
 
-    
-       
-       
+
+
